fef6ed374fb859afb42491e6c199ce0e44b067df
MartaPo82 <116483643+MartaPo82@users.noreply.github.com>
2022-12-29 18:35:37 +0200

    showing current temperature

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let city = "Lviv";
+let apiKey = "9e0fb79c2f66d0cd0dcf06710976a873";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemperature);
