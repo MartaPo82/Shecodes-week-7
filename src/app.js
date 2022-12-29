a04bb51d71730ad1afcbe0be20959dac42bc4ba6
MartaPo82 <116483643+MartaPo82@users.noreply.github.com>
2022-12-29 23:39:55 +0200

    adding icon from API

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ function showTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let city = "Lviv";
