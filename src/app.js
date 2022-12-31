5450a482bdcaae7b3156110248dab83fe7862256
MartaPo82 <116483643+MartaPo82@users.noreply.github.com>
2022-12-31 12:51:41 +0200

    adding celsius and fahrenheit units

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  celsiusTemperature = response.data.main.temp;
 }
 function search(event) {
   event.preventDefault();
@@ -52,3 +53,23 @@ if (minutes < 10) {
 }
 let dataValue = document.querySelector("#date");
 dataValue.innerHTML = `${day}, ${hour}:${minutes}`;
+
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusLink = document.querySelector("#celsius-link");
+fahrenheitLink.addEventListener("click", showCelsiusTemperature);
+
+let celsiusTemperature = null;
