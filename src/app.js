3eef22d9311bf2f421e5d20ec4769ed8537031f4
MartaPo82 <116483643+MartaPo82@users.noreply.github.com>
2023-01-05 23:54:09 +0200

    adding weather forecast

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function getForecast(coordinates) {
   let apiKey = "9e0fb79c2f66d0cd0dcf06710976a873";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -91,8 +91,6 @@ ${forecastDay.temp.max}
   });
   forecastElement.innerHTML = forecastHTML;
 }
-
-displayForecast();
 
 let celsiusTemperature = null;
 
